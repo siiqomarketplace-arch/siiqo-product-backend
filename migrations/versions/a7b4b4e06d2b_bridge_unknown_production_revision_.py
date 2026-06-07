3d5040ae942d87c4487528f7d64753351c2a0d88
Okereke <okerekeinno6@gmail.com>
2026-06-07 06:59:53 +0100

    Add product condition and location migrations

---
+++ migrations/versions/a7b4b4e06d2b_bridge_unknown_production_revision_.py
@@ -0,0 +1,24 @@
+""" Bridge unknown production revision a7b4b4e06d2b
+
+Revision ID: a7b4b4e06d2b
+Revises: c6ea6d7135fa
+Create Date: 2026-06-07 06:58:41.972106
+
+"""
+from alembic import op
+import sqlalchemy as sa
+
+
+# revision identifiers, used by Alembic.
+revision = 'a7b4b4e06d2b'
+down_revision = 'c6ea6d7135fa'
+branch_labels = None
+depends_on = None
+
+
+def upgrade():
+    pass
+
+
+def downgrade():
+    pass
